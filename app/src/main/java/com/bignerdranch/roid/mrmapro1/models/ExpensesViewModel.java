971bebb4e449021fae1889d43539c2ebdc32f914
Toxinlab <toxinlab@gmail.com>
2019-05-26 02:16:19 +0200

    - Did a doozy m8

--- app/src/main/java/com/bignerdranch/roid/mrmapro1/models/ExpensesViewModel.java
+++ app/src/main/java/com/bignerdranch/roid/mrmapro1/models/ExpensesViewModel.java
@@ -25,9 +25,13 @@ public class ExpensesViewModel extends ViewModel {
         return mExpensesLiveData;
     }
 
-
     public MutableLiveData<ArrayList<String>> getCategoriesLiveData() {
         return mCategoriesLiveData;
+    }
+
+    public void addExpense(ExpenseModel expense){
+        mExpensesList.add(expense);
+        mExpensesLiveData.setValue(mExpensesList);
     }
 
     public void addCategory(String category){
@@ -35,22 +39,58 @@ public class ExpensesViewModel extends ViewModel {
         mCategoriesLiveData.setValue(mCategoriesList);
     }
 
+    public void removeExpense(int position){
+        mExpensesList.remove(position);
+        mExpensesLiveData.setValue(mExpensesList);
+    }
+
     public void initModels(){
+        mCategoriesList.add("");
         mCategoriesList.add("State");
         mCategoriesList.add("Personal");
         mCategoriesList.add("Travel");
         mCategoriesLiveData.setValue(mCategoriesList);
 
-        //Log.e("OCAJAN POKUSAJ", "MAIN");
-
-
-        mExpensesList.add(new ExpenseModel("Poresko", 2000, new Date(), "State"));
-        mExpensesList.add(new ExpenseModel("Pljeskavica", 400, new Date(), "Personal"));
-        mExpensesList.add(new ExpenseModel("Kazna", 4500, new Date(), "State"));
-        mExpensesList.add(new ExpenseModel("Hedonizam", 80000, new Date(), "Personal"));
-        mExpensesList.add(new ExpenseModel("Bus", 300, new Date(), "Travel"));
+        mExpensesList.add(new ExpenseModel("Poresko", "2000", new Date(), "State"));
+        mExpensesList.add(new ExpenseModel("Pljeskavica", "400", new Date(), "Personal"));
+        mExpensesList.add(new ExpenseModel("Kazna", "4500", new Date(), "State"));
+        mExpensesList.add(new ExpenseModel("Hedonizam", "80000", new Date(), "Personal"));
+        mExpensesList.add(new ExpenseModel("Bus", "300", new Date(), "Travel"));
         mExpensesLiveData.setValue(mExpensesList);
 
+    }
+
+    public void filterExpenses(String filterString,String category){
+        ArrayList<ExpenseModel> tempExpenses = new ArrayList<>();
+        if(category==""){
+            for (int i = 0; i<mExpensesList.size();i++) {
+                if(mExpensesList.get(i).getName().startsWith(filterString)){
+                    tempExpenses.add(mExpensesList.get(i));
+                }
+            }
+        } else {
+            for (int i = 0; i<mExpensesList.size();i++) {
+                if(mExpensesList.get(i).getName().startsWith(filterString) && mExpensesList.get(i).getCategory().equals(category)){
+                    tempExpenses.add(mExpensesList.get(i));
+                }
+            }
+        }
+
+
+        mExpensesLiveData.setValue(tempExpenses);
+    }
+
+    public void filterCategories(String category){
+        if(category==""){
+            return;
+        }
+        ArrayList<ExpenseModel> tempExpenses = new ArrayList<>();
+        for (int i = 0; i<mExpensesList.size();i++) {
+            if(mExpensesList.get(i).getCategory().equals(category)){
+                tempExpenses.add(mExpensesList.get(i));
+            }
+        }
+        mExpensesLiveData.setValue(tempExpenses);
     }
 
 
